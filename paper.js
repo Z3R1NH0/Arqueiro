c31013613bc5ddaa7301ef35d499f400764d76ec
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-08-24 16:20:41 -0300

    Update paper.js

--- paper.js
+++ paper.js
@@ -9,12 +9,14 @@ class Paper{
             density: 0.1
         }
 
-        this.r = r;
+        
         this.x =x;
         this.y = y;
+        this.r = r;
 
-        this.body = Bodies.circle(x,y,this.r,options);
+        //ajuste do raio do papel para cair dentro da lixeira
         this.image = loadImage("paper.png");
+        this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
         World.add(world,this.body);
     }
 
@@ -22,8 +24,12 @@ class Paper{
      var pos = this.body.position;
 
      push();
+     //function translate moves a body by a given vector relative to its current position, without imparting any velocity.
+     translate(pos.x, pos.y);
+     rectMode(CENTER);
+     fill(255,0,255);
      imageMode(CENTER);
-     image(this.image,pos.x,pos.y,this.r,this.r);
+     image(this.image,0,0,this.r,this.r);
      pop();
     }
 }
